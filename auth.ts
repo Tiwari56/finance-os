0b8e140c4cb9804f69d96c71d0550675cfe81640
Nishit tiwari <nishit.panditkk@gmail.com>
2026-06-06 09:52:47 +0530

    Fix login 500: skip OAuth providers when env vars not set

--- auth.ts
+++ auth.ts
@@ -22,14 +22,12 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         signIn: "/login",
     },
     providers: [
-        Google({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        }),
-        GitHub({
-            clientId: process.env.GITHUB_CLIENT_ID!,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-        }),
+        ...(process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET
+            ? [Google({ clientId: process.env.GOOGLE_CLIENT_ID, clientSecret: process.env.GOOGLE_CLIENT_SECRET })]
+            : []),
+        ...(process.env.GITHUB_CLIENT_ID && process.env.GITHUB_CLIENT_SECRET
+            ? [GitHub({ clientId: process.env.GITHUB_CLIENT_ID, clientSecret: process.env.GITHUB_CLIENT_SECRET })]
+            : []),
         Credentials({
             name: "Email",
             credentials: {
